0570ec0667321442cd0ba09d35442738bee1c4af
Muhammadali0981 <muhammadali030605@gmail.com>
2024-09-01 15:26:43 +0500

    learned uses_header

---
+++ add.h
@@ -0,0 +1,10 @@
+#include <iostream>
+#ifndef ADD_H
+#define ADD_H
+
+using namespace std;
+
+
+int add(int a, int b){return (a+b);} 
+
+#endif
---
+++ uses_header.cpp
@@ -0,0 +1,8 @@
+#include "add.h"
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << add(3,2);
+}
