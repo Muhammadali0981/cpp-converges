0d7f0525bceed0e0811839adfc9adc7e7ea765e9
Muhammadali0981 <muhammadali030605@gmail.com>
2024-09-05 08:57:41 +0500

    :>

---
+++ insertion_sort.cpp
@@ -0,0 +1,33 @@
+/*
+    Name: Muhammmad Ali
+    Date: 05/09/2024
+    Desc: Insertion sort
+*/
+
+#include <iostream>
+
+using namespace std;
+
+void print_rec(int array[5], int i = 0) {
+    if (i >= 5) return;
+    cout << array[i] << endl;
+    return print_rec(array, i+1);
+}
+
+int main()
+{
+    int array[5] = {9,6,4,2,10};
+
+    for (int i = 1; i < 5; i++)
+    {
+        int j = i;
+        while (j >= 0 && array[j-1] > array[j]) {
+            swap(array[j-1], array[j]);
+            j--;
+        }
+    }
+
+    print_rec(array);
+    
+    return 0;
+}
