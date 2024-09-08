b3b0365b35ccf0fed2c66a47ced4dd7695d1f09a
Muhammadali0981 <muhammadali030605@gmail.com>
2024-09-08 18:58:39 +0500

    done

--- insertion_sort.cpp
+++ insertion_sort.cpp
@@ -18,13 +18,23 @@ int main()
 {
     int array[5] = {9,6,4,2,10};
 
-    for (int i = 1; i < 5; i++)
-    {
-        int j = i;
-        while (j >= 0 && array[j-1] > array[j]) {
-            swap(array[j-1], array[j]);
+    // for (int i = 1; i < 5; i++)
+    // {
+    //     int j = i;
+    //     while (j >= 0 && array[j-1] > array[j]) {
+    //         swap(array[j-1], array[j]);
+    //         j--;
+    //     }
+    // }
+
+    for (int i = 1; i < 5; ++i) {
+        int temp = array[i];
+        int j = i-1;
+        while (j >= 0 && array[j] > temp) {
+            array[j+1] = array[j];
             j--;
         }
+        array[j+1] = temp;
     }
 
     print_rec(array);
